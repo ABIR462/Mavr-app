7b8bbc982d12fc5f3fb96df69c07c60c9fc3b990
ABIR KUMAR KAYAL <akpaintings87@gmail.com>
2026-05-27 19:59:00 +0530

    update

--- .vercel/output/functions/__fallback.func/chunks/build/_tanstack-start-manifest_v-DyO8hD4T.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/User/Downloads/elegance-engine-main/elegance-engine-main/Mavr-app/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-DLJUahpD.js"] }, "/": { filePath: "C:/Users/User/Downloads/elegance-engine-main/elegance-engine-main/Mavr-app/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-CWQGRY78.js"] } }, clientEntry: "/assets/index-DLJUahpD.js" });
-export {
-  tsrStartManifest
-};
